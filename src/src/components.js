d229ef72b57957aba7f69574fde300598aa47eef
Marcos <jkmarcosb@gmail.com>
2024-03-07 18:08:00 -0600

    actualizar exportacioni de componentes

--- src/src/components.js
+++ src/src/components.js
@@ -1,5 +1,4 @@
-export { Hello } from "./atomico-hello/atomico-hello";
-export { Brand } from "./atomico-brand/atomico-brand";
-export { Counter } from "./atomico-counter/atomico-counter";
 export { Pomodoro } from "./atomico-pomodoro/atomico-pomodoro";
 export { SettingsPopup } from "./atomico-settings-popup/atomico-settings-popup";
+export { SettingsContext } from "./atomico-pomodoro-theme/atomico-pomodoro-theme"
+export { PomodoroCounter } from "./atomico-pomodoro-counter/atomico-pomodoro-counter"
